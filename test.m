868f85b546b29c5ed2fd52a0ae040dfec290b4fe
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2023-01-21 19:32:22 -0400

    update

--- test.m
+++ test.m
@@ -3,6 +3,8 @@
 % f1 = str2func(["@(x)" f1_str]);
 % f2 = str2func(["@(x)" f2_str]);
 n = 1000;
-f1_str = @(x) sqrt(x)+3;
-f2_str = @(x) 5-sqrt(1-x);
+f1_str = str2sym("sqrt(x)+3");
+% @(x) sqrt(x)+3;
+f2_str = str2sym("5-sqrt(1-x)");
+% @(x) 5-sqrt(1-x);
 composition(n, f1_str, f2_str);
